0ba81f3a33eba626ce62d733f83740c44a50eed2
r21185409 <39392292+r21185409@users.noreply.github.com>
2018-05-18 10:19:24 +0800

    first

--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int a,b;
+	int sum;
+	printf("a?,b?\n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	sum = a + b;
+	printf("%d\n",sum);	
 	printf("Hello,world");
 	return 0;
 }
